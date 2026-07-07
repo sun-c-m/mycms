912a8836b80e39e38f9d75fdd882b9b92f557a67
sun-c-m <3393297421@qq.com>
2026-07-07 22:57:13 +0800

    加入后端初始界面

--- mycms/src/main/java/cn/edu/guet/mycms/mapper/LoginMapper.java
+++ mycms/src/main/java/cn/edu/guet/mycms/mapper/LoginMapper.java
@@ -4,7 +4,6 @@ import cn.edu.guet.mycms.dto.LoginDTO;
 import cn.edu.guet.mycms.vo.UserLoginVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
-
 @Mapper
 public interface LoginMapper {
     UserLoginVO login(LoginDTO loginDTO);
