e5df757bf6ca9e9fea997c8e31ff3a10fce81509
sun-c-m <3393297421@qq.com>
2026-09-06 20:44:01 +0800

    加入数据库

--- mycms/src/main/resources/db/migration/V1.0.4__init_data.sql
+++ mycms/src/main/resources/db/migration/V1.0.4__init_data.sql
@@ -1,4 +1,4 @@
 
 
-INSERT INTO cms.user(username,password) VALUES('sun','7842509');
+INSERT INTO cms.user(username,password) VALUES('liwei','lw1234');
 INSERT INTO cms.user(username,password) VALUES('guet','guet1234');
